a7eaf12b88bc1237448658fa2e086af409ed1f77
dmtcr <dmtcrafter@gmail.com>
2022-04-06 15:00:05 -0600

    4/6/2022 2:59 PM DMT

--- timer.py
+++ timer.py
@@ -1,36 +1,24 @@
 
 import time
 
-led_pin_A = 4
-
-led_pin_B = 9
-
-led_pin_C = 10
-
-led_pin_D = 11
-
 total_time = 10
 
 
-class LED:
+class LED(pin):
     ON = False
+    PIN_NUM = pin
 
-    def __init__(self):
+    def __init__(self, pin):
         super().__init__()
-        self.pin_num = ''
 
 
-Led_1 = LED()
-Led_1.pin_num = 4
+Led_1 = LED(pin=4)
 
-Led_2 = LED()
-Led_2.pin_num = 9
+Led_2 = LED(pin=9)
 
-Led_3 = LED()
-Led_3.pin_num = 10
+Led_3 = LED(pin=10)
 
-Led_4 = LED()
-Led_4.pin_num = 11
+Led_4 = LED(pin=11)
 
 running = True
 
@@ -39,8 +27,9 @@ total_time_2_4 = (total_time / 4) * 2
 total_time_3_4 = (total_time / 4) * 3
 
 while running:
+    total_time = total_time - 1
 
-    if total_time < 0:
+    if total_time > 0:
         if total_time == total_time_3_4:
             Led_1.ON = False
 
